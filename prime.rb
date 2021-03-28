bf58388f2ffd94aec3a4ba4b25243072be194cb3
langan.kg Langan <langan.kg@gmail.com>
2021-03-28 14:12:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 # Add  code here!
-#number? 2 answer.odd? return true that it is only divisible by itself and one, and also that it is greater than one. 
+#number? 2 answer.odd? return true that it is only divisible by itself and one, and also that it is greater than one.
 
 def prime_number(i)
 end
