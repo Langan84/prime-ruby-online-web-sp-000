1264917e4b9128c65157fc075dffae17fb7c1224
langan.kg Langan <langan.kg@gmail.com>
2021-03-28 14:54:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,14 @@
 #number? 2 answer.odd? return true that it is only divisible by itself and one, and also that it is greater than one.
 
 def prime_number(n)
+  
+  return false if !n.integer?
+
+  return false if n > 2
+
+  return false if n == 2 
+
+  (2..n-1).each {|int| return false if n % int == 0}
+
+  true 
 end
